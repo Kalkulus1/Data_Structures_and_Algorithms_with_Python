0602b04d81ed00ad4f69c8bc8a1ccd82ba687a78
unknown <mumunim10@gmail.com>
2019-05-15 00:38:07 +0000

    fsdkg with '#' will be ignored, and an empty message aborts the commit. ~ ~ ~ ~ ~ ~ <d_Algorithms_with_Python/.git/COMMIT_EDITMSG [unix] (00:38 15/05/2019)1,0-1 All <es_and_Algorithms_with_Python/.git/COMMIT_EDITMSG" [unix] 16L, 527C
    ~
    ~
    ~
    ~
    ~
    ~
    <d_Algorithms_with_Python/.git/COMMIT_EDITMSG [unix] (00:38 15/05/2019)1,0-1 All
    <es_and_Algorithms_with_Python/.git/COMMIT_EDITMSG" [unix] 16L, 527C

---
+++ Stacks_and_Queues/stacks_example.py
@@ -0,0 +1,41 @@
+# #Factorial code
+# def factorial(n):
+#     if n==0:
+#         return 1
+#     else:
+#         return n*factorial(n-1)
+# print(factorial(2))
+
+class Stack:
+
+    def __init__(self):
+        self.stack = []
+
+    def isEmpty(self):
+        return self.stack == []
+
+    def push(self,data):
+        self.stack.append(data)
+
+    def pop(self):
+        data = self.stack[-1]
+        del self.stack[-1]
+        return data
+
+    def peek(self):
+        return self.stack[0]
+
+    def sizeStack(self):
+        return len(self.stack)
+
+
+stack = Stack()
+stack.push(1)
+stack.push(2)
+stack.push(3)
+print(stack.sizeStack())
+print("pop", stack.pop())
+print("pop", stack.pop())
+print(stack.sizeStack())
+print("Peek", stack.peek())
+print(stack.sizeStack())
